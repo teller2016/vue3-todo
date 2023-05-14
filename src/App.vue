cb96073759dc5cbe3eba6825b950ba0801c3b4ef
teller2016 <teller75@naver.com>
2023-05-14 17:23:48 +0900

    feature: simple todo function added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <!-- header -->
+    <h2>TODO LIST</h2>
+
+    <!-- form -->
+    <form class="d-flex" @submit.prevent="onSubmit">
+      <!-- input -->
+      <div class="flex-grow-1 mr-2">
+        <input
+          class="form-control"
+          type="text"
+          v-model="todo"
+          placeholder="Type new TODO"
+        />
+      </div>
+
+      <!-- submit button -->
+      <div>
+        <button class="btn btn-primary" type="submit">Add</button>
+      </div>
+    </form>
+
+    <div class="card mt-2" v-for="todo in todos" :key="todo.id">
+      <div class="card-body p-2">
+        {{ todo.subject }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const todo = ref("");
+    const todos = ref([
+      {
+        id: 1,
+        subject: "todo1",
+      },
+      {
+        id: 2,
+        subject: "todo2",
+      },
+    ]);
+
+    const onSubmit = () => {
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value,
+      });
+    };
+
+    return {
+      todo,
+      todos,
+      onSubmit,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
