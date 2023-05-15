c325aceb716bb0d9d63548b62589b393e04d4f33
teller2016 <teller75@naver.com>
2023-05-15 23:10:37 +0900

    feature: - search function added - db added

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,31 @@
   <div class="container">
     <!-- header -->
     <h2>TODO LIST</h2>
+
+    <!-- 검색 Input -->
+    <input
+      class="form-control"
+      type="text"
+      v-model="searchText"
+      placeholder="검색"
+    />
+
+    <hr />
+
     <!-- TODO 추가 Form -->
     <TodoSimpleForm @add-todo="addTodo"></TodoSimpleForm>
 
+    <!-- ERROR -->
+    <div style="color: red">{{ error }}</div>
+
     <!-- empty case -->
-    <div v-if="!todos.length" style="color: blue" class="mt-2">
+    <div v-if="!filteredTodos.length" style="color: blue" class="mt-2">
       TODO가 없습니다
     </div>
 
     <!-- TODO 목록 -->
     <TodoList
-      :todos="todos"
+      :todos="filteredTodos"
       @delete-todo="deleteTodo"
       @toggle-completed="toggleCompleted"
     ></TodoList>
@@ -20,9 +34,10 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import TodoSimpleForm from "./components/TodoSimpleForm.vue";
 import TodoList from "./components/TodoList.vue";
+import axios from "axios";
 
 export default {
   components: {
@@ -44,9 +59,38 @@ export default {
       },
     ]);
 
+    // 에러
+    const error = ref("");
+
+    // 검색
+    const searchText = ref("");
+    const filteredTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter((todo) => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+
+      return todos.value;
+    });
+
     // TODO 추가 이벤트
     const addTodo = (todo) => {
-      todos.value.push(todo);
+      error.value = "";
+
+      // DB에 todo 저장
+      axios
+        .post("http://localhost:3000/todos", {
+          subject: todo.subject,
+          completed: todo.completed,
+        })
+        .then((res) => {
+          todos.value.push(res.data);
+        })
+        .catch((error) => {
+          error.value = "에러 발생";
+          console.log(error);
+        });
     };
 
     // TODO 완료 상태 toggle
@@ -61,9 +105,12 @@ export default {
 
     return {
       todos,
+      searchText,
+      filteredTodos,
       addTodo,
       toggleCompleted,
       deleteTodo,
+      error,
     };
   },
 };
