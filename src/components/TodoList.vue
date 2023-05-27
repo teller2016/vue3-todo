1122d697decf603a6b8fc8ceb4fe28a3dee5fe73
teller2016 <teller75@naver.com>
2023-05-28 00:11:05 +0900

    refactor: divided List Component from TodoList component

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,35 +1,38 @@
 <template>
   <!-- TODO 목록 -->
-  <div class="card mt-2" v-for="(todo, index) in todos" :key="todo.id">
-    <div
-      class="card-body p-2 d-flex align-items-center"
-      style="cursor: pointer"
-      @click="moveToPage(todo.id)"
-    >
-      <div class="flex-grow-1">
-        <!-- checkbox -->
-        <input
-          class="ml-2 mr-2"
-          type="checkbox"
-          :checked="todo.completed"
-          @change="toggleCompleted(index, $event)"
-          @click.stop
-        />
-        <span :class="{ todo: todo.completed }">
-          {{ todo.subject }}
-        </span>
+  <List :items="todos">
+
+    <template #default="{item, index}">
+      <div
+        class="card-body p-2 d-flex align-items-center"
+        style="cursor: pointer"
+        @click="moveToPage(item.id)"
+      >
+        <div class="flex-grow-1">
+          <!-- checkbox -->
+          <input
+            class="ml-2 mr-2"
+            type="checkbox"
+            :checked="item.completed"
+            @change="toggleCompleted(index, $event)"
+            @click.stop
+          />
+          <span :class="{ todo: item.completed }">
+            {{ item.subject }}
+          </span>
+        </div>
+        <div>
+          <button
+            type="button"
+            class="btn btn-danger btn-sm"
+            @click.stop="openModal(item.id)"
+          >
+            Delete
+          </button>
+        </div>
       </div>
-      <div>
-        <button
-          type="button"
-          class="btn btn-danger btn-sm"
-          @click.stop="openModal(todo.id)"
-        >
-          Delete
-        </button>
-      </div>
-    </div>
-  </div>
+    </template>
+  </List>
   
   <teleport to="#molly">
     <DeleteModal v-if="showModal" @close="closeModal" @delete="deleteTodo">
@@ -41,9 +44,12 @@
 import { useRouter } from "vue-router";
 import DeleteModal from '@/components/DeleteModal.vue';
 import {ref} from 'vue';
+import List from '@/components/List.vue';
+
 export default {
   components: {
     DeleteModal,
+    List,
   },
   props: {
     todos: {
